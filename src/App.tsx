1cc356fd6c883d775683c1a055c3066196b2f330
wariboko65 <oamienjoh20034@gmail.com>
2026-03-22 14:58:58 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Hero from './sections/Hero';
+import './App.css'
+
+function App() {
+    return (
+        <div className="appContainer">
+            <Hero />
+        </div>
+    )
+}
+
+export default App
---
+++ src/sections/Hero.tsx
@@ -0,0 +1,11 @@
+import "./Hero.css";
+
+function Hero() {
+    return (
+        <div className="heroContainer">
+            
+        </div>
+    )
+}
+
+export default Hero;

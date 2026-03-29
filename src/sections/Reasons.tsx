e7cf22c096d60b1498ea2f846c750749fe61fa24
wariboko65 <oamienjoh20034@gmail.com>
2026-03-29 07:40:56 +0100

    framer animation

--- src/sections/Reasons.tsx
+++ src/sections/Reasons.tsx
@@ -1,19 +1,36 @@
 import type { JSX } from "react";
 import "../sections/reasons.css";
 import { Clock, HeartHandshake, Shield } from "lucide-react";
+import {  motion } from "framer-motion";
 
 export default function Reasons(): JSX.Element {
     return (
         <section 
             aria-label="Reasons to help Jason"
             className="reasons-container"
+            
         >
             <h1 className="reasons-header">Why Jason Needs Your Help</h1>
             <p className="reasons-subtitle">
                 Born on November 3, 2016, Jason is a determined boy who deserves the chance to use his hands freely. Your support can make that possible.
             </p>
             <div className="card-container">
-                <div className="card">
+                <motion.div 
+                    className="card"
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0}}
+                    layout
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ layout: {
+                    type: "spring",
+                    bounce: 0.35,
+                    duration: 0.6
+                    },
+                    type: "spring",
+                    stiffness: 120,
+                    damping: 14
+                }}    
+                >
                     <div className="icon-container">
                         <HeartHandshake className="reasons-icon"/>
                     </div>
@@ -21,9 +38,24 @@ export default function Reasons(): JSX.Element {
                     <p>
                         Jason Chukwuemeka Odunna is a bright and resilient young boy whose life has been shaped by a congenital condition affecting both hands.
                     </p>
-                </div>
+                </motion.div>
 
-                <div className="card">
+                <motion.div 
+                    className="card"
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0}}
+                    layout
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ layout: {
+                    type: "spring",
+                    bounce: 0.35,
+                    duration: 0.6
+                    },
+                    type: "spring",
+                    stiffness: 120,
+                    damping: 14
+                }}  
+                >
                     <div className="icon-container">
                         <Clock className="reasons-icon"/>
                     </div>
@@ -31,9 +63,25 @@ export default function Reasons(): JSX.Element {
                     <p>
                         He faces daily challenges with writing, gripping objects, and performing basic tasks most children take for granted.
                     </p>
-                </div>
+                </motion.div>
 
-                <div className="card">
+                <motion.div 
+                    className="card"
+                    initial={{ opacity: 0, y: 50 }}
+                    whileInView={{ opacity: 1, y: 0}}
+                    layout
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ layout: {
+                    type: "spring",
+                    bounce: 0.35,
+                    duration: 0.6
+                    },
+                    type: "spring",
+                    stiffness: 120,
+                    damping: 14
+                }}  
+                >
+                    
                     <div className="icon-container">
                         <Shield className="reasons-icon"/>
                     </div>
@@ -41,7 +89,7 @@ export default function Reasons(): JSX.Element {
                     <p>
                         With timely corrective surgery, Jason has a strong chance at gaining functional use of his hands and living independently.
                     </p>
-                </div>
+                </motion.div>
             </div>
         </section>
     )

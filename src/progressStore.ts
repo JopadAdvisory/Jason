b0c43acad044cf49bd824b9189b868c1136e291e
wariboko65 <oamienjoh20034@gmail.com>
2026-03-30 02:10:00 +0100

    updated progress bar

--- src/progressStore.ts
+++ src/progressStore.ts
@@ -11,9 +11,9 @@ type ProgressStore = {
 export const useProgressStore = create<ProgressStore>()(
     persist (
         (set, get) => ({
-            donors: 94,
-            raised: 4_120_000,
-            goal: 20_000_000,
+            donors: 146,
+            raised: 23_000_000,
+            goal: 75_000_000,
             getPercentage: () => {
                 const { raised, goal } = get();
                 if (goal === 0) return 0;
